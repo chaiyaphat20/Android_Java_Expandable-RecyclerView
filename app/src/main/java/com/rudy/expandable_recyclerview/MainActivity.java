e0851bf9bc1089d99b82ca3d39a3cbf07175ef07
chaipat supharak <chaipat.sup@cpf.co.th>
2024-01-18 22:11:54 +0700

    feat: add binding

--- app/src/main/java/com/rudy/expandable_recyclerview/MainActivity.java
+++ app/src/main/java/com/rudy/expandable_recyclerview/MainActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.rudy.expandable_recyclerview.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
